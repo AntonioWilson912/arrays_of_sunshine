eab7c2c1588d925a69c64710143e25d623110ef8
Opt3dRx <96031147+Opt3dRx@users.noreply.github.com>
2022-11-11 17:58:40 -0500

    Added a few app routes

--- flask_app/controllers/employees.py
+++ flask_app/controllers/employees.py
@@ -1,6 +1,23 @@
 from flask_app import app
 from flask_app.models import employee, role
+from flask import render_template,redirect,request,session,flash
 
 @app.route("/")
 def index():
     return "App started!"
+
+
+@app.route("/register-employee" , methods = ['POST'])
+def register_employee():
+    pass
+
+@app.route("/login-employee", methods = ['POST'])
+def login_employee():
+    if employee.validate_login(request.form):
+    
+        return render_template('')
+    pass
+
+@app.route("/delete-employee")
+def delete_employee(): 
+    pass
