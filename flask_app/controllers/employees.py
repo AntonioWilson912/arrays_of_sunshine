dcc6d4ca1dd81dc0bacad3223037d95e1d9716c2
Opt3dRx <96031147+Opt3dRx@users.noreply.github.com>
2022-11-11 22:42:11 -0500

    Addes some controllers and encryptions

--- flask_app/controllers/employees.py
+++ flask_app/controllers/employees.py
@@ -4,20 +4,44 @@ from flask import render_template,redirect,request,session,flash
 
 @app.route("/")
 def index():
-    return "App started!"
+    
+    return render_template("index.html")
 
 
 @app.route("/register-employee" , methods = ['POST'])
 def register_employee():
-    pass
+    if employee.Employee.validate_register_employee(request.form):
+        employee.Employee.create_employee(request.form)
+        session['id'] = employee.Employee.id
+        return render_template('dashboard_employee.html', employee = employee.Employee)
+    
+    return request.referrer
+
 
 @app.route("/login-employee", methods = ['POST'])
 def login_employee():
-    if employee.validate_login(request.form):
+    if employee.Employee.validate_login_employee(request.form):
+        employee.Employee.get_employee_by_email(request.form)
+        session['id'] = employee.Employee.id
+        return render_template('dashboard_employee.html', employee = employee.Employee)
     
-        return render_template('')
-    pass
+    else: 
+        return request.referrer
+
+
+@app.route("/edit-employee", methods = ['POST'])
+def edit_employee(id):
+    if 'id' not in session:
+        return redirect("/")
+    data = { "id" : id}
+    employee.Employee.delete_employee(data)
+    
 
 @app.route("/delete-employee")
-def delete_employee(): 
-    pass
+def delete_employee(id):
+    if 'id' not in session:
+        return redirect("/")
+    data = { "id" : id}
+    employee.Employee.delete_employee(data)
+    
+    return "success", 200
