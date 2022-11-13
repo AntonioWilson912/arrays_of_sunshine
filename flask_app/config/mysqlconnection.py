ca198640f304b7147f27451703db37cfb6f7cbbb
Opt3dRx <96031147+Opt3dRx@users.noreply.github.com>
2022-11-13 13:19:51 -0500

    added .env for sql password

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -1,12 +1,17 @@
 # a cursor is the object we use to interact with the database
 import pymysql.cursors
 # this class will give us an instance of a connection to our database
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 class MySQLConnection:
 	def __init__(self, db):
 		# change the user and password as needed
 		connection = pymysql.connect(host = 'localhost',
 									user = 'root',
-									password = 'root',
+									assword = os.getenv('sql_password'), 
 									db = db,
 									charset = 'utf8mb4',
 									cursorclass = pymysql.cursors.DictCursor,
